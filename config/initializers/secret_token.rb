c19cd37d29690a34016d1dfe4d6b0526dd5dc244
Artem Aminov <artemaminov@gmail.com>
2012-09-18 22:46:49 +0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarstyleLocal::Application.config.secret_token = '66ed5a1b6708f6168aa7911cb52bd17bccad03af2cfdd5fd793f4934d465cf53991784dd94d6f2b9aa1f1d1a15bc1fd872ede2ce8bc6cc08ba997e051dc54838'
