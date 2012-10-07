50006ad2737556ffcc1bd187b458118dbebfc764
Artem Aminov <artemaminov@gmail.com>
2012-10-07 21:46:03 +0600

    Permalink routing

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,25 @@
 class IndexController < ApplicationController
 
-  def index
-		page = Page.find_by_permalink(params[:permalink])
-		if page
-			@body = page
-			render 'index/_page'
+  def home
+		# Rendering home page
+
+	end
+
+	def show_page
+		# Rendering desired page if found
+		page_permalink = params[:page_permalink]
+		if page_permalink
+			page = Page.find_by_permalink(page_permalink)
+			if page
+				@body = page
+				render 'index/_page'
+			end
+
+		# Show default page according section
+		else
+
 		end
+
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,9 +55,9 @@ BarstyleLocal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'index#index'
+  root :to => 'index#home'
 
-	match '/:section(/:permalink)' => 'index#index', :as => :section
+	match ':section(/:page_permalink)' => 'index#show_page', :as => :section
 
   # See how all your routes lay out with "rake routes"
 
