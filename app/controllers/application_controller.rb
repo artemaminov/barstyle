d60be5c37ec35d214f618e3e438cfc30ac2c43a8
Artem Aminov <artemaminov@gmail.com>
2012-10-26 16:35:14 +0600

    Heroku 404 handle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,14 @@ class ApplicationController < ActionController::Base
 
 	protect_from_forgery
 
-	unless Rails.application.config.consider_all_requests_local
-		rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound, with: :render_404
-	end
+	rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound, with: :render_404
 
 
 	private
 
 	def render_404
 		@title = t('errors.default')
+		@menu_sections = Section.list
 		render template: "app/_not_found", layout: 'layouts/application', status: :not_found
 	end
 
